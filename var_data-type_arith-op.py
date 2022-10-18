d8863e64784f63c4307e40d594e0d5945d980b4c
Sanjida Kabir <sanjida.kabir.srity@gmail.com>
2022-10-18 17:53:39 +0600

    var dataType arithmetic operator

---
+++ var_data-type_arith-op.py
@@ -0,0 +1,58 @@
+# Variable
+text_to_print = "Hello World"
+print(text_to_print)
+
+number1 = 10
+number2 = 5
+result = number1 + number2
+print(result)
+
+number1 = 5
+number1 = 8
+print(number1)
+
+
+# Data Type
+v = 10
+print(type(v))
+v = "10"
+print(type(v))
+v = 10.0
+print(type(v))
+
+# String 
+s = "100"
+print(s)
+s = "nvdfnd 12324 ,. <> ?88"
+print(s)
+s = ' 34ffj'
+print(s)
+s = ""
+print(s)
+s = ''
+print(s)
+
+divide = 5/2
+print(divide)
+interval = 5-2
+print(interval)
+multiplication = 5*2
+print (multiplication)
+quotient = 5//2
+print(quotient)
+reminder = 5%2
+print(reminder)
+spuare = 5**2
+print(spuare)
+
+
+# Some built-in function
+
+print(number1, number2, "Hi", text_to_print)
+
+name = input("What is your name? ")
+print("Welcome to Python,", name, "!")
+
+number1 = int(input("Please type an integer and press enter: "))
+number2 = int(input("Please type another integer and press enter: "))
+print("number1 + number2 =", number1 + number2)
